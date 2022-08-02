c6166ca7e1850b1e58a0453a9f47fb8f50fc6bf3
IsmaelPSY <ismaelpsy.12@gmail.com>
2022-08-02 14:24:12 -0500

    Ahora si

---
+++ src/database/migrations/20220630154704-create-users.js
@@ -0,0 +1,50 @@
+'use strict';
+
+module.exports = {
+    async up(queryInterface, DataTypes) {
+        /**
+         * Add altering commands here.
+         *
+         * Example:
+         * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+         */
+
+        await queryInterface.createTable('users', {
+            uuid: {
+                type: DataTypes.UUID,
+                allowNull: false,
+                primaryKey: true
+            },
+            name: {
+                type: DataTypes.STRING,
+                allowNull: false
+            },
+            email: {
+                type: DataTypes.STRING,
+                allowNull: false,
+                unique: "users_email_key"
+            },
+            password: {
+                type: DataTypes.STRING,
+                allowNull: false
+            },
+        });
+        await queryInterface.addConstraint(
+          'users', //nombre de la tabla
+          {
+            fields: ['email', 'uuid'],//columnas que tendran esta restriccion
+            type: 'unique', //restriccion para que los valores sean unicos
+            name: 'unique_emai_uuid' //nombre para guardar el cambio
+          }
+        )
+    },
+
+    async down(queryInterface, Sequelize) {
+        /**
+         * Add reverting commands here.
+         *
+         * Example:
+         * await queryInterface.dropTable('users');
+         */
+    }
+};
--- src/database/models/init-models.js
+++ src/database/models/init-models.js
@@ -1,6 +1,22 @@
 var DataTypes = require("sequelize").DataTypes;
 var _users = require("./users");
 
+const Sequelize = require('sequelize');
+require('dotenv').config()
+const env = process.env.NODE_ENV || 'development';
+const config = require('../config/config');
+
+const configObj = config[env]
+
+let sequelize;
+
+if (configObj.use_env_variable) {
+  sequelize = new Sequelize('postgres://amzjavqgumqusl:498dc6e0e7307d3f3c960a6e8232e8a1e564170363d7a9a23b935cb807628ce0@ec2-54-208-104-27.compute-1.amazonaws.com:5432/dc6s9h32v7ssvr', configObj);
+}
+else {
+  sequelize = new Sequelize(configObj.database, configObj.username, configObj.password, configObj);
+}
+
 function initModels(sequelize) {
   var users = _users(sequelize, DataTypes);
 
@@ -12,3 +28,5 @@ function initModels(sequelize) {
 module.exports = initModels;
 module.exports.initModels = initModels;
 module.exports.default = initModels;
+module.exports.sequelize = sequelize;
+
